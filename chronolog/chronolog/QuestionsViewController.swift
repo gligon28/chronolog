1095d7097a1faff6d17d233033a5fd2bb7d84538
janie <jgironlo@stedwards.edu>
2024-09-23 00:18:48 -0500

    add questions to activities

---
+++ chronolog/chronolog/QuestionsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuestionsViewController.swift
+//  chronolog
+//
+//  Created by Janie Giron on 9/22/24.
+//
+
+import UIKit
+
+class QuestionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
