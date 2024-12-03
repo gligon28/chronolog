bdfbaa6c0650d78c3f500fc56cbdb75077cee1a9
janie <jgironlo@stedwards.edu>
2024-12-03 13:41:39 -0600

    Create AIConflictResolver class- takes in all existing events and new event and returns all events resolved. Add extension to CustomEvent struct for JSON encoding/decoding used in AIConflictResolver

--- chronolog/chronolog/CustomEvent.swift
+++ chronolog/chronolog/CustomEvent.swift
@@ -26,4 +26,59 @@ struct CustomEvent {
     var allowSplit: Bool
     var allowOverlap: Bool
     var priority: Priority
+    
+}
+
+// Add an extension for JSON encoding/decoding
+extension CustomEvent: Codable {
+    enum CodingKeys: String, CodingKey {
+        case title
+        case date
+        case startTime
+        case endTime
+        case duration
+        case description
+        case isRecurring
+        case daysOfWeek
+        case isAllDay
+        case allowSplit = "isSplitable"
+        case allowOverlap
+        case priority
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(title, forKey: .title)
+        try container.encodeIfPresent(date, forKey: .date)
+        try container.encodeIfPresent(startTime, forKey: .startTime)
+        try container.encodeIfPresent(endTime, forKey: .endTime)
+        try container.encode(duration, forKey: .duration)
+        try container.encode(description, forKey: .description)
+        try container.encode(isRecurring, forKey: .isRecurring)
+        try container.encodeIfPresent(daysOfWeek, forKey: .daysOfWeek)
+        try container.encode(isAllDay, forKey: .isAllDay)
+        try container.encode(allowSplit, forKey: .allowSplit)
+        try container.encode(allowOverlap, forKey: .allowOverlap)
+        try container.encode(priority.rawValue, forKey: .priority)
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        title = try container.decode(String.self, forKey: .title)
+        date = try container.decodeIfPresent(Date.self, forKey: .date)
+        startTime = try container.decodeIfPresent(Date.self, forKey: .startTime)
+        endTime = try container.decodeIfPresent(Date.self, forKey: .endTime)
+        duration = try container.decode(Int.self, forKey: .duration)
+        description = try container.decode([String].self, forKey: .description)
+        isRecurring = try container.decode(Bool.self, forKey: .isRecurring)
+        daysOfWeek = try container.decodeIfPresent([String: Bool].self, forKey: .daysOfWeek)
+        isAllDay = try container.decode(Bool.self, forKey: .isAllDay)
+        allowSplit = try container.decode(Bool.self, forKey: .allowSplit)
+        allowOverlap = try container.decode(Bool.self, forKey: .allowOverlap)
+        
+        let priorityString = try container.decode(String.self, forKey: .priority)
+        priority = Priority(rawValue: priorityString) ?? .low
+    }
 }
